1a7f8a182d6065753f8968238abd8af448eb2bf3
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2023-08-06 10:00:00 +0100

    feat: Add product data array for product listing

--- vite-project/src/home/Home.jsx
+++ vite-project/src/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
 import HomeCategory from './HomeCategory';
+import CategoryShowcase from './CategoryShowcase';
 
 const Home = () => {
     return(
         <div>
             <Banner/>
             <HomeCategory/>
+            <CategoryShowcase/>
         </div>
     );
 };
